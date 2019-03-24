d9d78ac2469a08d20d949ab86ef3377f79221d5f
B-Tarik <tarik.bouchekkif@gmail.com>
2019-03-24 21:52:21 +0000

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -11,43 +11,137 @@
 var expect = require('chai').expect;
 var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectId;
-const MONGODB_CONNECTION_STRING = process.env.DB;
+const Joi = require('joi');
+
+const URL = process.env.DB;
 //Example connection: MongoClient.connect(MONGODB_CONNECTION_STRING, function(err, db) {});
+
+function validate(req) {
+  const schema = {
+    title : Joi.string().min(3).max(50).required()
+  };
+  return Joi.validate(req, schema);
+}
+
+async function connect() {
+  const db = await MongoClient.connect(URL)
+  return db.collection('books');
+}
 
 module.exports = function (app) {
 
   app.route('/api/books')
-    .get(function (req, res){
-      //response will be array of book objects
-      //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+    .get(async (req, res) => {
+      try {
+        const collection = await connect();
+        let books = await collection.find().toArray()
+
+        books = books.map(elm => {
+          elm.commentcount = elm.comments.length
+          delete elm.comments;
+          return elm
+        })
+
+        res.json(books);
+      } catch(err) {
+        res.json(err)
+      }
     })
     
-    .post(function (req, res){
-      var title = req.body.title;
-      //response will contain new book object including atleast _id and title
+    .post(async (req, res) => {
+      try{
+        const title = req.body.title;
+        if(!title) return res.send('missing title');
+
+        const { error } = validate({title: title});
+        if (error) return res.send(error.details[0].message);
+
+        const collection = await connect();
+
+        const doc = {title:title, comments:[]};
+        const book = await collection.insert(doc)
+
+        if(!book) return res.send('sorry could not add book. try again! thanks')
+        res.json(book.ops[0]);
+        
+      } catch(err) {
+        res.json(err)
+      }
     })
     
-    .delete(function(req, res){
-      //if successful response will be 'complete delete successful'
+    .delete(async (req, res) => {
+    try {
+      const collection = await connect();
+      const drop = await collection.drop();
+      
+      if (!drop) return res.send('sorry could not delete all book. try again! thanks');
+      res.send("complete delete successful");
+      
+    } catch(err) {
+      res.json(err)
+    }
     });
 
 
 
   app.route('/api/books/:id')
-    .get(function (req, res){
-      var bookid = req.params.id;
-      //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+    .get(async (req, res) => {
+      try {
+        let id = req.params.id;
+
+        if(!id) return res.send('please enter an id.');
+        id = new ObjectId(id); 
+
+        const collection = await connect();
+        const book = await collection.find({_id:id}).toArray()
+        
+        if(book.length === 0) return res.send('no book exists');
+        res.json(book[0]);
+        
+      } catch(err) {
+          res.json(err)
+      }
     })
     
-    .post(function(req, res){
-      var bookid = req.params.id;
-      var comment = req.body.comment;
-      //json res format same as .get
+    .post(async (req, res) => {
+      try {
+        const comment = req.body.comment;
+        let id = req.params.id;
+
+        if(!id || !comment) return res.send('please enter an id and comment');
+        id = new ObjectId(id);
+
+        const collection = await connect();
+        const updatedBook = await collection.findAndModify(
+          {_id: id},
+          {},
+          {$push: { comments: comment }},
+          {new: true, upsert: false}
+        )
+        if(!updatedBook) return res.send('sorry could not add comment. try again! thanks')  
+        res.json(updatedBook.value);
+
+      } catch(err) {
+        res.json(err)
+      }
     })
     
-    .delete(function(req, res){
-      var bookid = req.params.id;
-      //if successful response will be 'delete successful'
+    .delete(async (req, res) => {
+      try {
+        let id = req.params.id;
+
+        if(!id) return res.send('please enter an id');
+        id = new ObjectId(id); 
+
+        const collection = await connect();
+        const deletedBook = collection.findOneAndDelete({_id:id})
+
+        if (!deletedBook) return res.send('could not delete '+ id);
+        res.send('delete successful') 
+
+      } catch(err) {
+        res.json(err)
+      }
     });
   
 };
